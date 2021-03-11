a827de10670a9d5c1e9d76a0e02f065314968c7f
nukemain <sowa.piotr15@gmail.com>
2021-03-11 18:04:59 +0100

    bugfixes

--- lightclient.py
+++ lightclient.py
@@ -1,47 +1,96 @@
 import socket
 from SpotifyAPI import MySpotifyAPI
 import time
+import sys
+
 API = MySpotifyAPI()
 songname = ''
 oldsongname = ''
 i = 0
+'''
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("IP_HERE", 1025))
+'''
+
 try:
     while True:
         time.sleep(1)
         API.call_token_refresh("C:\\Users\\Sowap\\MySpotifyAPITokenVault.txt")
         print(API.get_currently_playing_songs_name()+"_1")
-        songname = API.get_currently_playing_songs_name()
-        if(songname == oldsongname):
-            i = i + 1
-            print("pause_" + str(i))
-            oldsongname = songname
-        else:
-            print(API.download_image_of_currently_playing_song("C:\\Users\\Sowap\\Desktop\\test\\currently_playing_songs_img")+"_2")
-            colors = API.dominant_color("C:\\Users\\Sowap\\Desktop\\test\\currently_playing_songs_img.png")
-            #print(API.most_common_used_color())
-            red = round(int(colors[0]))
-            green = round(int(colors[1]))
-            blue = round(int(colors[2]))
-
-            if(red > 255):
-                red = 255
-            if(green > 255):
-                green = 255
-            if(blue > 255):
-                blue = 255   
+        if(API.get_currently_playing_songs_name == "JSONERROR_JSONERROR"):
             try:
-                s.connect(("192.168.0.100", 1025))
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect(("IP_HERE", 1025))
+                sendData = "0_0_0_PC_ALERTR_3"
+                s.send(sendData.encode())
+                # ERROR FLASHING
+                # ERROR FLASHING
+                # ERROR FLASHING
+                # ERROR FLASHING
+                # ERROR FLASHING
             except TimeoutError:
                 print("TimeoutError: could not connect to pi")
                 break
-            additional_argument = "NONE"
-            sendData = str(red)+"_"+str(green)+"_"+str(blue)+"_PC_"+additional_argument
-            s.send(sendData.encode())
-            oldsongname = songname
+        else:
+            songname = API.get_currently_playing_songs_name()
+            if(songname == oldsongname):
+                i = i + 1
+                print("pause_" + str(i))
+                oldsongname = songname
+            else:
+                if(API.download_image_of_currently_playing_song("C:\\Users\\Sowap\\Desktop\\test\\currently_playing_songs_img") == "ERROR_NOTLISTENING"):
+                    print("you are not listening to anything")
+                    try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect(("IP_HERE", 1025))
+                        sendData = "0_0_0_PC_ALERTR_3"
+                        s.send(sendData.encode())
+                        
+                        # ERROR FLASHING
+                        # ERROR FLASHING
+                        # ERROR FLASHING
+                        # ERROR FLASHING
+                        # ERROR FLASHING
+                    except TimeoutError:
+                        print("TimeoutError: could not connect to pi")
+                        break
+                else:
+                    colors = API.dominant_color("C:\\Users\\Sowap\\Desktop\\test\\currently_playing_songs_img.png")
+                    #print(API.most_common_used_color())
+                    red = round(int(colors[0]))
+                    green = round(int(colors[1]))
+                    blue = round(int(colors[2]))
+
+                    if(red > 255):
+                        red = 255
+                    if(green > 255):
+                        green = 255
+                    if(blue > 255):
+                        blue = 255   
+
+                    try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect(("IP_HERE", 1025))
+                        print("Sending color data")
+                    except TimeoutError:
+                        print("TimeoutError: could not connect to pi")
+                        break
+                    additional_argument = "NONE"
+                    additional_argument_value = "0"
+                    sendData = str(red)+"_"+str(green)+"_"+str(blue)+"_PC_"+additional_argument+"_"+additional_argument_value
+                    s.send(sendData.encode())
+                    oldsongname = songname
+
 except KeyboardInterrupt:
     print('Interrupted')
-    s.connect(("192.168.0.100", 1025))
-    sendData = "0_0_0_PC_CLOSE"
-    s.send(sendData.encode())
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("IP_HERE", 1025))
+        #sendData = "0_0_0_PC_CLOSE_0"
+        sendData = "0_0_0_PC_ALERTC_1"
+        s.send(sendData.encode())
+        sys.exit()
+    except TimeoutError:
+        print("TimeoutError: could not connect to pi")
 
+sys.exit() 
--- serverlightcontrol.py
+++ serverlightcontrol.py
@@ -2,39 +2,112 @@ import time
 import pigpio
 import socket
 import sys
+from gpiozero import Button
+from signal import signal, SIGTERM, SIGHUP
 
 pigs = pigpio.pi()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.bind((socket.gethostname(), 1234))
 print(socket.gethostname())
 s.bind(("192.168.0.100", 1025))
 
+
+
+
+
+def shutdown():
+    print("SHUTDOWN")
+    red_alert(3)
+    turn_off_lights()
+    print("shutting down")
+    sys.exit()
+
 def turn_off_lights():
+    print("turn_off_lights")
     pigs.set_PWM_dutycycle(17, 0)
     pigs.set_PWM_dutycycle(22, 0)
     pigs.set_PWM_dutycycle(24, 0)
 
+def red_alert(flashes):
+    print("red alert: "+str(flashes))
+    flashesb = flashes
+    pigs.set_PWM_dutycycle(17, 0)
+    pigs.set_PWM_dutycycle(22, 0)
+    pigs.set_PWM_dutycycle(24, 0)
+    i = 0
+    f = 0
+    while True:
+        if(i < flashesb):
+            if(f == 0):
+                f = 1
+                i = i + 1
+                pigs.set_PWM_dutycycle(17, 255)
+                time.sleep(1)
+            else:
+                f = 0
+                pigs.set_PWM_dutycycle(17, 0)
+                time.sleep(1)
+        else:
+            break
+    pigs.set_PWM_dutycycle(17, 0)
+
+def safe_exit():
+    print("safe_exit")
+    pigs.set_PWM_dutycycle(17, 0)
+    pigs.set_PWM_dutycycle(22, 0)
+    pigs.set_PWM_dutycycle(24, 0)
+    exit(1)
+
+
+button = Button(26)
+button.when_pressed = shutdown
+signal(SIGTERM, safe_exit)
+signal(SIGHUP, safe_exit)
+
 while True:
-    # now our endpoint knows about the OTHER endpoint.
-    s.listen(5)
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    rcvdData = clientsocket.recv(1024).decode()
-    rcvdTable = rcvdData.split("_")
-    print(rcvdTable)
+    try:
+        # now our endpoint knows about the OTHER endpoint.
+        s.listen(5)
+        clientsocket, address = s.accept()
+        print(f"Connection from {address} has been established.")
+        rcvdData = clientsocket.recv(1024).decode()
+        rlen = len(rcvdData)
+        if(rlen==0):
+            print("pc's fucked")
+            turn_off_lights()
+            sys.exit()
+        else:
+            rcvdTable = rcvdData.split("_")
+            print(rcvdTable)
 
-    brightnessR = rcvdTable[0] #pin 17
-    brightnessG = rcvdTable[1] #pin 22
-    brightnessB = rcvdTable[2] #pin 24
+            brightnessR = rcvdTable[0] #pin 17
+            brightnessG = rcvdTable[1] #pin 22
+            brightnessB = rcvdTable[2] #pin 24
 
-    username = rcvdTable[3]
-    additional_info = rcvdTable[4]
+            username = rcvdTable[3]
+            additional_info = rcvdTable[4]
+            additional_info_value = rcvdTable[5]
 
-    if(str(additional_info) == "CLOSE"):
-        print("closing")
+            if(str(additional_info) == "CLOSE"):
+                print("closing")
+                turn_off_lights()
+                sys.exit()
+
+            if(str(additional_info) == "ALERT"):
+                red_alert(int(additional_info_value))
+            if(str(additional_info) == "ALERTC"):
+                red_alert(int(additional_info_value))
+                sys.exit()
+            else:
+                pigs.set_PWM_dutycycle(17, int(brightnessR))
+                pigs.set_PWM_dutycycle(22, int(brightnessG))
+                pigs.set_PWM_dutycycle(24, int(brightnessB))
+    except KeyboardInterrupt:
         turn_off_lights()
         sys.exit()
-
-    pigs.set_PWM_dutycycle(17, int(brightnessR))
-    pigs.set_PWM_dutycycle(22, int(brightnessG))
-    pigs.set_PWM_dutycycle(24, int(brightnessB))
+'''
+pigs.set_PWM_dutycycle(17, 0)
+pigs.set_PWM_dutycycle(22, 0)
+pigs.set_PWM_dutycycle(24, 0)
+'''
